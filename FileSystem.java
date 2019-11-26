2a9606a45946024c811f59b0ec2fd30136aa33b5
Tabitha Roemish <tabitharoemish@hotmail.com>
2019-11-25 19:24:44 -0800

    Create FileSystem.java

---
+++ FileSystem.java
@@ -0,0 +1,35 @@
+import java.util*;
+
+public class FileSystem extends Threads
+{
+
+  public Filesystem (  )
+  {
+  
+  }
+ int read( int fd, byte buffer[] )
+ {
+ 
+ }
+
+ int write( int fd, byte buffer[] );
+  {
+  }
+  
+  int seek( int fd, int offset, int whence )
+  {
+  }
+  
+  int close( int fd )
+  {
+  }
+  
+int delete( String fileName )
+  {
+  }
+  
+int fsize( int fd )
+  {
+  }
+  
+}
