43c82bc6d6e1e8ac88b2aec00547b6cee4662df5
Tabitha Roemish <tabitharoemish@hotmail.com>
2019-11-25 19:14:45 -0800

    Update TCB.java

--- TCB.java
+++ TCB.java
@@ -1,14 +1,9 @@
 public class TCB {
 
    private Thread thread = null;
-
    private int tid = 0;
-
    private int pid = 0;
-
    private boolean terminate = false;
-
-
 
    // User file descriptor table: 
 
@@ -16,28 +11,17 @@ public class TCB {
 
    public FileTableEntry[] ftEnt = null;
 
-
-
    public TCB( Thread newThread, int myTid, int parentTid ) {
-
       thread = newThread;
-
       tid = myTid;
-
       pid = parentTid;
-
       terminated = false;
-
-
 
       // The following code is added for the file system
 
       ftEnt = new FileTableEntry[32];
-
       for ( int i = 0; i < 32; i++ )
-
          ftEnt[i] = null;         // all entries initialized to null
-
          // fd[0], fd[1], and fd[2] are kept null.
 
    }
